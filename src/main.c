82812c0a15a7fb0a6b90a8b3de44dade9c7caa2c
/usr/share <d1337r@post.ru>
2015-05-11 13:00:56 +0300

    FOV, moving, opening doors.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <curses.h>
+
+#include "mapmode.h"
 
 int main(int argc, char** argv) {
 
--- src/mapmode.h
+++ src/mapmode.h
@@ -1,100 +1,15 @@
 #ifndef MAPMODE_H
 #define MAPMODE_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
-#define MAX_ENTITIES 128
-#define MAP_WIDTH 80
-#define MAP_HEIGHT 20
+#include "mapdefs.h"
+#include "map_ai.h"
 
-enum maptypes {
+int draw_map(struct t_map* map, struct t_map_entity* persp);
 
-	MT_EXTERIOR,
-	MT_INTERIOR,
-	MT_ELEMENT_COUNT
+int mapgetch();
 
-};
-
-enum mapflags {
-
-	MF_SYMMETRIC = 1,
-	MF_SECURE = 2,
-	MF_PARANOID = 4,	
-};
-
-enum terraintypes {
-	TT_OUTSIDE,
-	TT_GRASS,
-	TT_CORRIDOR,
-	TT_SPACE,
-	TT_RESTRICTED_SPACE,
-	TT_SPECIAL_SPACE,
-	TT_WALL,
-	TT_WINDOW,
-	TT_DOOR,
-	TT_BARS,
-	TT_TABLE,
-	TT_CUSTOM,
-	TT_STAIRS,
-	TT_UNKNOWN,
-	TT_ELEMENT_COUNT
-};
-
-enum terrainflags {
-
-	TF_SOLID = 1,
-	TF_BLOCKS_VISION = 2,
-	TF_BLOCKS_SOUND = 4,
-	TF_BLOCKS_ATTACKS = 8,
-	TF_DODGE = 16,
-	TF_NOSPAWN = 32,
-	TF_OUTSIDE = 64,
-};
-
-enum entitytypes {
-	ET_NONE = 0,
-	ET_PLAYER,
-};
-
-enum spawnpos {
-
-	SF_DEFAULT = 0,
-	SF_RANDOM,
-	SF_RANDOM_INSIDE,
-	SF_RANDOM_RESTRICTED,
-	SF_RANDOM_SPECIAL,
-};
-
-struct t_square {
-	enum terraintypes type;
-	uint16_t hp;
-};
-
-struct t_map;
-struct t_map_entity;
-
-typedef uint16_t (*turnFunc)(struct t_map* map, struct t_map_entity* me);
-typedef uint16_t (*useFunc)(struct t_map* map, struct t_map_entity* me, struct t_map_entity* user);
-
-
-struct t_map_entity {
-	enum entitytypes type;
-	uint16_t x;
-	uint16_t y;
-	uint16_t twait; ///< turns left to wait
-	turnFunc turn;
-	useFunc use;
-};
-
-struct t_map {
-	struct t_square sq[MAP_WIDTH*MAP_HEIGHT];
-	
-	uint8_t width;
-	uint8_t height;
-
-	struct t_map_entity ent[MAX_ENTITIES];
-};
-
-int draw_map(struct t_map* map);
-
+int mapmode();
 #endif
