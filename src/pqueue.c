767a854cacafefb5448061e150a7d45bc8f4e1bb
/usr/share <d1337r@post.ru>
2015-05-13 18:14:14 +0300

    derp 2

---
+++ src/pqueue.c
@@ -0,0 +1,59 @@
+#include "pqueue.h"
+#include <limits.h>
+
+struct pqel* pq_find_empty_element (struct pqel* array, size_t sz) {
+
+	for (int i=0; i < sz; i++)
+		if (array[i].v == NULL) return &array[i];
+	return NULL;
+}
+
+int pq_add_element(struct pqel* array, size_t sz, void* ptr, int priority) {
+
+	struct pqel* newel = pq_find_empty_element(array,sz);
+	if (newel == NULL) return 1;
+
+	newel->p = priority;
+	newel->v = ptr;
+	return 0;
+}
+
+void* pq_get_lowest(struct pqel* array, size_t sz) {
+
+	int lowest_p = INT_MAX; void* lowest_v = NULL; int lowest_i = -1;
+	for (int i=0; i < sz; i++)
+		if ((array[i].v != NULL) && (array[i].p < lowest_p)) {
+			lowest_p = array[i].p;
+			lowest_v = array[i].v;
+			lowest_i = i;
+		}
+	
+	if (lowest_i != -1) array[lowest_i].v = NULL;
+	return lowest_v;
+}
+
+int pq_decrease_priority(struct pqel* array, size_t sz, void* ptr, int newpriority) {
+	
+	for (int i=0; i < sz; i++)
+		if (array[i].v == ptr) {
+			array[i].p = newpriority; return 0; }
+
+	return 1;
+}
+
+int pq_decrease_or_add(struct pqel* array, size_t sz, void* ptr, int priority) {
+	
+	for (int i=0; i < sz; i++)
+		if (array[i].v == ptr) {
+			array[i].p = priority; return 0; }
+
+	return 2 + (pq_add_element(array,sz,ptr,priority));
+}
+
+
+int pq_empty (struct pqel* array, size_t sz) {
+	for (int i=0; i < sz; i++)
+		if (array[i].v != NULL) return 0;
+
+	return 1;
+}
---
+++ src/pqueue.h
@@ -0,0 +1,21 @@
+#ifndef PQUEUE_H
+#define PQUEUE_H
+#include <unistd.h>
+// an insane idea for a statically-allocated priority queue.
+
+struct pqel {
+	int p;
+	void* v;
+};
+
+int pq_add_element(struct pqel* array, size_t sz, void* ptr, int priority);
+
+void* pq_get_lowest (struct pqel* array, size_t sz);
+
+int pq_decrease_priority (struct pqel* array, size_t sz, void* ptr, int newpriority);
+
+int pq_decrease_or_add (struct pqel* array, size_t sz, void* ptr, int priority);
+
+int pq_empty (struct pqel* array, size_t sz);
+
+#endif
